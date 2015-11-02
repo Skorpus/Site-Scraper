ccf454571a1b154f3ac01a4a010c8e4a7a50797b
Dylan Maccora <dmaccora@localhost.localdomain>
2015-11-02 13:29:21 +1100

    Added only external links and tidied up concurrency

--- Scraper.java
+++ Scraper.java
@@ -43,13 +43,9 @@ public class Scraper {
 					String line;
 					// Need .* as can have any number of characters in front
 					// We know links will be of the form <a href="...">
-<<<<<<< HEAD
+
 					String linkPattern = "(.*)([hH][rR][eE][fF])=\"(.+?)\"";
-=======
-					// Use the *? to be non-greedy(reluctant) hence only goes to
-					// first match
-					String linkPattern = "(.*)([hH][rR][eE][fF])=\"(.*?)\"";
->>>>>>> master
+
 					// Pattern is a nice class that compiles the REGEX
 					Pattern pattern = Pattern.compile(linkPattern);
 
@@ -66,17 +62,11 @@ public class Scraper {
 							String output = line.substring(matcher.start(), matcher.end());
 							// Still not there yet, need to split the string and
 							// get the url
-<<<<<<< HEAD
 							output = output.split("href=\"")[1];
 							// And remove trailing \"
 							output = output.substring(0, output.length() - 1);
-=======
 							// System.out.println(output);
-							output = output.split("href=\"")[1];
-							// Just need to remove trailing \"
-							output = output.substring(0, output.length() - 1);
-							// System.out.println(output);
->>>>>>> master
+
 							// Next we want to put this into a set
 							allLinks.add(output);
 							// System.out.println("Added to set");
@@ -103,16 +93,16 @@ public class Scraper {
 	 * @return set of all links
 	 */
 	public Set<String> getLinks() {
-		System.out.print("Fetching...");
 		// Wait for thread to complete
 		try {
 			contentThread.join();
+			return allLinks;
 		} catch (InterruptedException e) {
 			System.out.println("Thread failed");
 			e.printStackTrace();
+			return null;
 		}
-		System.out.println("");
-		return allLinks;
+
 	}
 
 	/**
@@ -122,18 +112,55 @@ public class Scraper {
 	 * @return set of web links
 	 */
 	public Set<String> getWebLinks() {
-		// Make regex for links
-		Pattern pattern = Pattern.compile("(.*).((html)|(php)|(htm)|(com))");
-		Set<String> result = new LinkedHashSet<String>();
-		for (String link : allLinks) {
-			// Check each of the links found
-			Matcher matcher = pattern.matcher(link);
-			if (matcher.lookingAt()) {
-				result.add(link);
+
+		try {
+
+			contentThread.join();
+			// Make regex for links
+			Pattern pattern = Pattern.compile("(.*).((html)|(php)|(htm)|(com))");
+			Set<String> result = new LinkedHashSet<String>();
+			for (String link : allLinks) {
+				// Check each of the links found
+				Matcher matcher = pattern.matcher(link);
+				if (matcher.lookingAt()) {
+					result.add(link);
+				}
 			}
+			return result;
+		} catch (InterruptedException e) {
+			System.out.println("This sh*t cray");
+			return null;
 		}
 
-		return result;
+	}
+
+	/**
+	 * Here will only return external links found on the page
+	 * 
+	 * @return
+	 */
+	public Set<String> getExternalLinks() {
+		try {
+
+			contentThread.join();
+			Set<String> result = new LinkedHashSet<String>();
+			// Could just call the weblink method and then further refine but
+			// this
+			// would mean several more iterations, rather will just change the
+			// regex
+			// pattern on the original one
+			Pattern pattern = Pattern.compile("^http");
+			for (String link : allLinks) {
+				Matcher matcher = pattern.matcher(link);
+				if (matcher.lookingAt()) {
+					result.add(link);
+				}
+			}
+			return result;
+		} catch (InterruptedException e) {
+			System.out.println("This sh*t cray");
+			return null;
+		}
 	}
 
 }
